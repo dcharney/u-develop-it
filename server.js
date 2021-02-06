aeeebea7ebdf25ff1045a8025c9e534b39d22aef
dcharney <delaneycharney@gmail.com>
2021-02-07 00:14:04 +0100

    GET requests joined candidates and parties

--- server.js
+++ server.js
@@ -18,15 +18,13 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello world'
-    });
-});
-
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -43,8 +41,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates
-                    WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                    AS party_name 
+                    FROM candidates 
+                    LEFT JOIN parties 
+                    ON candidates.party_id = parties.id 
+                    WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
