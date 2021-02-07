ef3f723143d0634bd7a6a125b92d2e6492cf1957
dcharney <delaneycharney@gmail.com>
2021-02-07 02:06:38 +0100

    all working

--- server.js
+++ server.js
@@ -81,10 +81,21 @@ app.post('/api/candidate/', ({ body }, res) => {
         res.status(400).json({ error: errors });
         return;
     }
-    res.json({
-        message: 'success',
-        data: body,
-        id: this.lastID
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
     });
 });
 
